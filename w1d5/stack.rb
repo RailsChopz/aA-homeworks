7a07fb91b3fbd5bd4bda3e787038d22e1b8ac8bb
Joonho Syn <joonho.syn@gmail.com>
2018-06-21 23:02:46 -0700

    Fix spacing for readbility

--- w1d5/stack.rb
+++ w1d5/stack.rb
@@ -1,21 +1,24 @@
 class Stack
-    def initialize
-      # create ivar to store stack here!
-      @underlying_arr = []
-    end
-
-    def push(el)
-      # adds an element to the stack
-      underlying_arr.push(el)
-    end
-
-    def pop
-      # removes one element from the stack
-      underlying_arr.pop(el)
-    end
-
-    def peek
-      # returns, but doesn't remove, the top element in the stack
-      underlying_arr.last
-    end
+  def initialize
+    # create ivar to store stack here!
+    @underlying_arr = []
   end
+
+  def push(el)
+    # adds an element to the stack
+    underlying_arr.push(el)
+  end
+
+  def pop
+    # removes one element from the stack
+    underlying_arr.pop(el)
+  end
+
+  def peek
+    # returns, but doesn't remove, the top element in the stack
+    underlying_arr.last
+  end
+
+  private
+  attr_reader :underlying_arr
+end
